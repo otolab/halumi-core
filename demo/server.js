65160b902b394c2ffbe46ee50600fd9f2a5dfa86
Naoto Kato <naoto.kato@plaid.co.jp>
2017-07-28 04:34:31 +0900

    fix empty text reply

--- demo/server.js
+++ demo/server.js
@@ -11,7 +11,7 @@ startAsyncParser()
 
 app.get('/', function(req, res) {
   const text = req.query['text']
-  if (!text) return res.sendStatus(400)
+  if (!text) return res.send('???')
 
   res.setHeader('Access-Control-Allow-Origin', ACCESS_CONTROL_ALLOW_ORIGIN)
 
