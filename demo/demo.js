812db1699e9ff8891bfc42b1cc4d7bda10225e3c
Naoto Kato <naoto.kato@plaid.co.jp>
2017-07-27 14:23:48 +0900

    add demo server

---
+++ demo/demo.js
@@ -0,0 +1,126 @@
+
+const {comprehend} = require('../libs')
+
+const patterns = {
+  remote: [
+    ['リモート', 'する'],
+    ['リモート']
+  ],
+  rest: [
+    ['休み', 'する'],
+    ['休み', '思う'],
+    ['休む', '予定'],
+    ['休む', '思う'],
+    ['休む'],
+    ['休み']
+  ],
+  voyage: [
+    ['旅行', '*', '行く'],
+    ['旅行', '*', '予定']
+  ]
+}
+
+comprehend("明日は休みたい。", patterns, {debug: true}, (err, commands) => {
+  printCommands(commands)
+})
+
+comprehend("やっぱり休みます", patterns, {debug: true}, (err, commands) => {
+  printCommands(commands)
+})
+
+comprehend("明日は遠くに旅行にいく予定なのでリモートです", patterns, {debug: true}, (err, commands) => {
+  console.log('RESULT:', commands)
+  printCommands(commands)
+})
+
+comprehend("明日と、8/1, 2018年7月20-23日と、2017年7月の30, 31日はリモート作業です", patterns, {debug: true}, (err, commands) => {
+  printCommands(commands)
+})
+
+comprehend("8月1日まで旅行です。その間休みます", patterns, {debug: true}, (err, commands) => {
+  printCommands(commands)
+})
+
+comprehend("8月1日はリモートです。8月8日はお休みします。", patterns, {debug: true}, (err, commands) => {
+  printCommands(commands)
+})
+
+comprehend("8月1日までリモートなのでお休みします。", patterns, {debug: true}, (err, commands) => {
+  printCommands(commands)
+})
+
+comprehend("8月1日はリモートです。お休みします。", patterns, {debug: true}, (err, commands) => {
+  printCommands(commands)
+})
+
+comprehend("お休みします。8/1に。", patterns, {debug: true}, (err, commands) => {
+  printCommands(commands)
+})
+
+
+
+function printCommands(commands) {
+    let zatsudan = true
+    if (commands.length === 0) return;
+
+    console.log('------------')
+
+    console.log('input:', commands[0].source)
+
+    commands.forEach((command) => {
+      if (command.command === 'voyage') {
+        if (command.status.length == 0) {
+          zatsudan = false;
+          console.log("おお、いいですね。いってらっしゃいマシ！！");
+        }
+      }
+
+      if (command.command === 'rest') {
+        if (command.status.length === 0) {
+          zatsudan = false;
+          console.log("休暇申請デスネ。了解デス！")
+
+          if (command.days.length > 0) {
+            command.days.forEach((day) => {
+              console.log(' ', day.format('%Y年%m月%d日'))
+            })
+            console.log("に休暇を入れますデスヨ");
+          }
+          if (command.days.length === 0) {
+            console.log('いつデスか！？解らなかったデス！')
+          }
+        }
+        else if (command.status.length > 0) {
+          zatsudan = false;
+          console.log("休暇申請デスカ？ちょっと理解できなかったので、もう一度言ってくださいデスネ。")
+        }
+      }
+
+      if (command.command === 'remote') {
+        if (command.status.length == 0) {
+          zatsudan = false;
+          console.log("了解デス！");
+
+          if (command.days.length > 0) {
+            command.days.forEach((day) => {
+              console.log(' ', day.format('%Y年%m月%d日'))
+            })
+            console.log("にリモート作業日を入れますデスヨ");
+          }
+          if (command.days.length === 0) {
+            console.log('いつデスか！？解らなかったデス！')
+          }
+        }
+        else if (command.status.length > 0) {
+          zatsudan = false;
+          console.log("リモート申請デスカ？ちょっと理解できなかったので、もう一度言ってくださいデスネ。")
+        }
+      }
+
+    })
+
+    if (zatsudan) {
+      console.log("（雑談モードデスヨ）");
+    }
+
+}
---
+++ demo/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+
+const {comprehend, dequeue, startAsyncParser} = require('../libs')
+const patterns = require('./patterns')
+const {buildReply} = require('./reply')
+
+const ACCESS_CONTROL_ALLOW_ORIGIN = process.env.ACCESS_CONTROL_ALLOW_ORIGIN || '*'
+
+startAsyncParser()
+
+app.get('/', function(req, res) {
+  const text = req.query['text']
+  if (!text) res.sendStatus(400)
+
+  res.setHeader('Access-Control-Allow-Origin', ACCESS_CONTROL_ALLOW_ORIGIN)
+
+  let aborted = false
+  let timeout
+
+  if (text.length > 80) {
+    res.status(400).send('テキストが長すぎます')
+    return
+  }
+
+  const request = comprehend(text, patterns, {withQueue: true}, (err, commands) => {
+    clearTimeout(timeout)
+    if (err) return res.sendStatus(400)
+    if (!aborted) res.send(buildReply(commands))
+  })
+
+  timeout = setTimeout(() => {
+    dequeue(request)
+    aborted = true
+    res.sendStatus(504)
+  }, 5000)
+})
+
+app.listen(8080)
