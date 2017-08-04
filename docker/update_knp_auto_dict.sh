2f7fa4af9e5ed6a8dc7d1ebc282ac8328e948b5d
Naoto Kato <naoto.kato@plaid.co.jp>
2017-08-04 17:59:19 +0900

    support user knp dic

---
+++ docker/update_knp_auto_dict.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASE_DIR="/home/ubuntu/knp_dict"
+CMD="${BASE_DIR}/make_db"
+
+LANG=C
+gzip -dkq $BASE_DIR/*.gz
+sort $BASE_DIR/*.dat $* | uniq | sudo $CMD /usr/local/share/knp/dict/auto/auto.db -append \|
+for name in $(ls $BASE_DIR/*.gz); do
+  rm ${name%%.gz}
+done
